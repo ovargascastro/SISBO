7b84a04aac452b3db2164b68522936be9f123244
Jeaustin <axelts.97@gmail.com>
2019-09-22 23:05:26 -0600

    dao OC

---
+++ test/test.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import logic.*;
+import data.*;
+
+public class test {
+
+    public static void main(String[] args) {
+        OrdenCompraDAO OCDAO = new OrdenCompraDAO();
+        List<SboTbOrdenCompra> lista = new ArrayList<SboTbOrdenCompra>();
+        lista = OCDAO.listaFamilias("");
+        for (SboTbOrdenCompra u : lista) {
+            System.out.println(u.getOcIdPk());
+        }
+
+    }
+}
