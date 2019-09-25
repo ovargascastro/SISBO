87cdaf9717b32db4b27c93fff452649268d26103
Jeaustin <axelts.97@gmail.com>
2019-09-24 21:55:59 -0600

    algo

--- test/test.java
+++ test/test.java
@@ -7,12 +7,12 @@ import data.*;
 public class test {
 
     public static void main(String[] args) {
-        OrdenCompraDAO OCDAO = new OrdenCompraDAO();
-        List<SboTbOrdenCompra> lista = new ArrayList<SboTbOrdenCompra>();
-        lista = OCDAO.listaFamilias("");
-        for (SboTbOrdenCompra u : lista) {
-            System.out.println(u.getOcIdPk());
-        }
+//        OrdenCompraDAO OCDAO = new OrdenCompraDAO();
+//        List<SboTbOrdenCompra> lista = new ArrayList<SboTbOrdenCompra>();
+//        lista = OCDAO.listaFamilias("");
+//        for (SboTbOrdenCompra u : lista) {
+//            System.out.println(u.getOcIdPk());
+//        }
 
     }
 }
