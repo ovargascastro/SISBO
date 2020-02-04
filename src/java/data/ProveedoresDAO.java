bdc617893e9d0321890bd67fd2440d471394c186
Jeaustin <axelts.97@gmail.com>
2020-02-03 19:58:21 -0600

    DAOS mios

--- src/java/data/ProveedoresDAO.java
+++ src/java/data/ProveedoresDAO.java
@@ -28,10 +28,10 @@ public class ProveedoresDAO {
         try {
             AbaaTbProveedor ob = new AbaaTbProveedor();
             ob.setProveIdProvePk(Integer.parseInt(rs.getString("Prove_Id_Prove_PK")));
-            ob.setProveCodigo(rs.getString("Prove_Codigo"));
-            ob.setProveCedula(rs.getString("Prove_Cedula"));
-            ob.setProveTelefono(Integer.parseInt(rs.getString("Prove_Telefono")));
-            ob.setProveCorreo(rs.getString("Prove_Correo"));
+            ob.setProveCodigo(rs.getString("Prove_Codi"));
+            ob.setProveCedula(rs.getString("Prove_Cedu"));
+            ob.setProveTelefono(Integer.parseInt(rs.getString("Prove_Tele")));
+            ob.setProveCorreo(rs.getString("Prove_Corre"));
             ob.setProveFax(rs.getString("Prove_Fax"));
             ob.setProveNomb(rs.getString("Prove_Nomb"));
             return ob;
@@ -43,7 +43,7 @@ public class ProveedoresDAO {
     public List<AbaaTbProveedor> listaProveedor() {
         List<AbaaTbProveedor> resultado = new ArrayList<AbaaTbProveedor>();
         try {
-            String sql = "select * from ABAA_TB_Proveedor";
+            String sql = "select * from ABAA_TB_Prove";
             sql = String.format(sql);
             ResultSet rs = db.executeQuery(sql);
             while (rs.next()) {
@@ -57,7 +57,7 @@ public class ProveedoresDAO {
 //        public List<AbaaTbProveedor> listaProveedorFiltro(String filtro) {
 //        List<AbaaTbProveedor> resultado = new ArrayList<AbaaTbProveedor>();
 //        try {
-//            String sql = "select * from ABAA_TB_Proveedor p where p.Prove_Nomb like '%%%s%%'";
+//            String sql = "select * from ABAA_TB_Prove p where p.Prove_Nomb like '%%%s%%'";
 //            sql = String.format(sql, filtro);
 //            ResultSet rs = db.executeQuery(sql);
 //            while (rs.next()) {
@@ -73,7 +73,7 @@ public class ProveedoresDAO {
             public List<AbaaTbProveedor> listaProveedorFiltro(String filtro) {
         List<AbaaTbProveedor> resultado = new ArrayList<AbaaTbProveedor>();
         try {
-            String sql = "select * from ABAA_TB_Proveedor p where p.Prove_Nomb like '%%%s%%'";
+            String sql = "select * from ABAA_TB_Prove p where p.Prove_Nomb like '%%%s%%'";
             sql = String.format(sql, filtro);
             ResultSet rs = db.executeQuery(sql);
             while (rs.next()) {
@@ -85,7 +85,7 @@ public class ProveedoresDAO {
     }
             
            public void actualizarProveedor(AbaaTbProveedor objeto) throws Exception {
-        String query = "update ABAA_TB_Proveedor set Prove_Codigo = ?, Prove_Cedula = ?, Prove_Telefono = ?, Prove_Correo = ?, "
+        String query = "update ABAA_TB_Prove set Prove_Codi = ?, Prove_Cedu = ?, Prove_Tele = ?, Prove_Corre = ?, "
                 + "Prove_Fax = ?, Prove_Nomb = ? where Prove_Id_Prove_PK = ?";
         PreparedStatement preparedStmt = db.getConnection().prepareStatement(query);
         preparedStmt.setString(1, objeto.getProveCodigo());
@@ -101,20 +101,20 @@ public class ProveedoresDAO {
            
     
     public AbaaTbProveedor getProveedor(int id) throws Exception {
-        String sql = "select * from ABAA_TB_Proveedor where Prove_Id_Prove_PK='%s'";
+        String sql = "select * from ABAA_TB_Prove where Prove_Id_Prove_PK='%s'";
         sql = String.format(sql, id);
         ResultSet rs = db.executeQuery(sql);
         if (rs.next()) {
             return proveedor(rs);
         } else {
-            throw new Exception("Solicitud no Existe");
+            throw new Exception("Proveedor no Existe");
         }
     }
     
     
     
         public void crearProveedor(AbaaTbProveedor objeto) throws Exception{
-        String query = "insert into ABAA_TB_Proveedor(Prove_Codigo,Prove_Cedula,Prove_Telefono,Prove_Correo,Prove_Fax,Prove_Nomb)values(?,?,?,?,?,?)";
+        String query = "insert into ABAA_TB_Prove(Prove_Codi,Prove_Cedu,Prove_Tele,Prove_Corre,Prove_Fax,Prove_Nomb)values(?,?,?,?,?,?)";
         PreparedStatement preparedStmt = db.getConnection().prepareStatement(query);
        
         preparedStmt.setString(1, objeto.getProveCodigo());
